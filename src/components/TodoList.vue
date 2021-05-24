5e04d7a4d30512744d32e4cc1446aef9329fd69e
Alex <kushale2014@gmail.com>
2021-05-24 10:22:40 +0300

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <TodoItem
+        v-for="(todo, i) of todos"
+        v-bind:todo="todo"
+        v-bind:index="i"
+        v-on:remove-todo="removeToDo"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+export default {
+  props: ["todos"],
+  components: {
+    TodoItem,
+  },
+  methods: {
+    removeToDo(id) {
+      this.$emit("remove-todo", id);
+    },
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque enim
+      obcaecati illo, ut recusandae harum qui quibusdam officia eius, alias,
+      nobis ad exercitationem eveniet eligendi ratione animi quod? Aut, illo.
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
